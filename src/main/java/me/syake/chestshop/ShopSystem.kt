ab190dc39cd41369095b7b15abcca908cc95f351
shake <shake4837@gmail.com>
2020-11-16 02:05:03 +0900

    update

--- src/main/java/me/syake/chestshop/ShopSystem.kt
+++ src/main/java/me/syake/chestshop/ShopSystem.kt
@@ -9,11 +9,155 @@ import org.bukkit.block.BlockFace
 import org.bukkit.block.Chest
 import org.bukkit.block.Sign
 import org.bukkit.block.data.type.WallSign
+import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import java.util.*
 
 class ShopSystem(private val main: ChestShop) {
+    fun delItemInventory(inv: Inventory, item: ItemStack, amo: Int):Boolean {
+        item.amount = 1
+        var amount = 0
+        val items = mutableListOf<ItemStack>()
+        for(invItem in inv.contents) {
+            if(invItem==null) {
+                continue
+            }
+            val copyItem = invItem.clone()
+            copyItem.amount = 1
+            if(copyItem == item) {
+                items.add(invItem)
+                amount += invItem.amount
+            }
+        }
+        if(amount < amo) {
+            return false
+        }
+        amount = amo
+        for (i in items) {
+            if(i.amount==amount) {
+                i.amount = 0
+                return true
+            } else if(i.amount>amount) {
+                i.amount -= amount
+                return true
+            } else {
+                amount -= i.amount
+                i.amount = 0
+            }
+        }
+        return false
+    }
+    fun hasItem(inv: Inventory, item: ItemStack, amo: Int): Boolean {
+        item.amount = 1
+        var amount = 0
+        val items = mutableListOf<ItemStack>()
+        for(invItem in inv.contents) {
+            if(invItem==null) {
+                continue
+            }
+            val copyItem = invItem.clone()
+            copyItem.amount = 1
+            if(copyItem == item) {
+                items.add(invItem)
+                amount += invItem.amount
+            }
+        }
+        if(amount < amo) {
+            return false
+        }
+        return true
+    }
+    fun hasInventoryArea(inv: Inventory, item: ItemStack, amo: Int): Boolean {
+        item.amount = 1
+        var amount = 0
+        val items = mutableListOf<ItemStack>()
+        for (i in inv.contents) {
+            if (i == null) {
+                amount += item.maxStackSize
+                continue
+            }
+            val copyItem = i.clone()
+            copyItem.amount = 1
+            if (copyItem == item) {
+                amount += 64 - i.amount
+                items.add(i)
+            }
+        }
+        if (amount < amo) {
+            return false
+        }
+        return true
+    }
+    fun addItemInventory(inv: Inventory, item: ItemStack, amo: Int): Boolean {
+        item.amount = 1
+        var amount = 0
+        val items = mutableListOf<ItemStack>()
+        for(i in inv.contents) {
+            if(i == null) {
+                amount+=item.maxStackSize
+                continue
+            }
+            val copyItem = i.clone()
+            copyItem.amount = 1
+            if(copyItem == item) {
+                amount += 64-i.amount
+                items.add(i)
+            }
+        }
+        if(amount<amo) {
+            return false
+        }
+        amount = amo
+        Bukkit.broadcastMessage(amo.toString())
+        for(i in items) {
+            if(i.amount==i.maxStackSize) {
+                continue
+            }
+            if(item.maxStackSize-i.amount==amount) {
+                i.amount = item.maxStackSize
+                return true
+            } else if(item.maxStackSize-i.amount>amount) {
+                i.amount+=amount
+                return true
+            } else {
+                amount-=i.amount
+                i.amount = item.maxStackSize
+            }
+        }
+        while (true) {
+            if(amount==item.maxStackSize) {
+                item.amount = item.maxStackSize
+                inv.addItem(item)
+                return true
+            } else if(amount<item.maxStackSize) {
+                item.amount = amount
+                inv.addItem(item)
+                return true
+            } else {
+                item.amount = item.maxStackSize
+                inv.addItem(item)
+                amount -= item.maxStackSize
+            }
+        }
+    }
+    fun dropItem(player: Player, item: ItemStack, amo: Int) {
+        var amount = amo
+        while (true) {
+            if (amount > item.maxStackSize) {
+                item.amount = item.maxStackSize
+                amount -= item.maxStackSize
+                val dropItem = player.location.world!!.dropItemNaturally(player.location, item)
+                dropItem.pickupDelay = 0
+            } else {
+                item.amount = amount
+                val dropItem = player.location.world!!.dropItemNaturally(player.location, item)
+                dropItem.pickupDelay = 0
+                break
+            }
+        }
+    }
     fun onSignRightClick(event: PlayerInteractEvent, block: Block, blockState: Sign, data: WallSign) {
         if(!(blockState.getLine(0)=="§e§lSHOP"||blockState.getLine(0)=="§c§lAdmin SHOP")) {
             return
@@ -47,92 +191,37 @@ class ShopSystem(private val main: ChestShop) {
             TODO("今後作成します。")
         }
         val item = main.shops.config().getItemStack("${chest.location.world?.name}-${chest.location.blockX}-${chest.location.blockY}-${chest.location.blockZ}.item")
-        var amount = main.shops.config().getInt("${chest.location.world?.name}-${chest.location.blockX}-${chest.location.blockY}-${chest.location.blockZ}.amount")
+        val amount = main.shops.config().getInt("${chest.location.world?.name}-${chest.location.blockX}-${chest.location.blockY}-${chest.location.blockZ}.amount")
         val buyPrice = main.shops.config().getInt("${chest.location.world?.name}-${chest.location.blockX}-${chest.location.blockY}-${chest.location.blockZ}.price")
         item?.amount = 1
         if(blockState.getLine(0)=="§c§lAdmin SHOP") {
-            while (true) {
-                if (amount > item!!.maxStackSize) {
-                    item.amount = item.maxStackSize
-                    amount -= item.maxStackSize
-                    val dropItem = event.player.location.world!!.dropItemNaturally(event.player.location, item)
-                    dropItem.pickupDelay = 0
-                } else {
-                    item.amount = amount
-                    val dropItem = event.player.location.world!!.dropItemNaturally(event.player.location, item)
-                    dropItem.pickupDelay = 0
-                    break
-                }
-            }
+            dropItem(event.player, item!!, amount)
             return
         }
-        var amountTwo = amount
-        var itemCount = 0
-        for (i in chest.inventory.contents) {
-            if (i == null) {
-                continue
+        val emerald = ItemStack(Material.EMERALD)
+        if(main.economy) {
+            if(!(main.econ!!.has(Bukkit.getOfflinePlayer(event.player.uniqueId) ,buyPrice.toDouble()))) {
+                event.player.sendMessage(main.lang.toMessage("balanceInsufficient", "Your balance is insufficient."))
+                return
             }
-            val invItem = i.clone()
-            val invItemCount = invItem.amount
-            invItem.amount = 1
-            if (invItem == item) {
-                itemCount += invItemCount
-            }
-        }
-        if(itemCount>=amount) {
-            if(main.economy) {
-                if(!(main.econ!!.has(Bukkit.getOfflinePlayer(event.player.uniqueId) ,buyPrice.toDouble()))) {
-                    event.player.sendMessage(main.lang.toMessage("balanceInsufficient", "Your balance is insufficient."))
-                    return
-                }
-                main.econ!!.withdrawPlayer(Bukkit.getOfflinePlayer(event.player.uniqueId), buyPrice.toDouble())
-                main.econ!!.depositPlayer(Bukkit.getOfflinePlayer(UUID.fromString(owner)), (buyPrice.toDouble()/100)*(100-main.config.getInt("tax", 0)))
-            } else {
-                if(!main.withdrawItem.withdraw(event.player, ItemStack(Material.EMERALD), buyPrice)) {
-                    event.player.sendMessage(main.lang.toMessage("emeraldInsufficient", "Your emerald is insufficient."))
-                    return
-                }
-            }
-            while (true) {
-                if (amount > item!!.maxStackSize) {
-                    item.amount = item.maxStackSize
-                    amount -= item.maxStackSize
-                    val dropItem = event.player.location.world!!.dropItemNaturally(event.player.location, item)
-                    dropItem.pickupDelay = 0
-                } else {
-                    item.amount = amount
-                    val dropItem = event.player.location.world!!.dropItemNaturally(event.player.location, item)
-                    dropItem.pickupDelay = 0
-                    break
-                }
-            }
-            for (i in chest.inventory.contents) {
-                if (i == null) {
-                    continue
-                }
-                item.amount = i.amount
-                if (i == item) {
-                    when {
-                        i.amount == amountTwo -> {
-                            i.amount = 0
-                            chest.update()
-                            return
-                        }
-                        i.amount <= amountTwo -> {
-                            amountTwo -= i.amount
-                            i.amount = 0
-                        }
-                        i.amount >= amountTwo -> {
-                            i.amount -= amountTwo
-                            chest.update()
-                            return
-                        }
-                    }
-                }
-            }
+            main.econ!!.withdrawPlayer(Bukkit.getOfflinePlayer(event.player.uniqueId), buyPrice.toDouble())
+            main.econ!!.depositPlayer(Bukkit.getOfflinePlayer(UUID.fromString(owner)), (buyPrice.toDouble()/100)*(100-main.config.getInt("tax", 0)))
+            dropItem(event.player, item!!, amount)
         } else {
-            event.player.sendMessage(main.lang.toMessage("outOfStock", "This shop does not have it in stock. Please wait until it's refilled or contact us at %player%.").replace("%player%", Bukkit.getOfflinePlayer(UUID.fromString(owner)).name!!))
-            return
+            if(hasInventoryArea(chest.inventory, emerald, buyPrice)) {
+                if(hasItem(chest.inventory, item!!, amount)) {
+                    delItemInventory(chest.snapshotInventory, item, amount)
+                    addItemInventory(chest.snapshotInventory, emerald, buyPrice)
+                    dropItem(event.player, item, amount)
+                    chest.update()
+                } else {
+                    event.player.sendMessage(main.lang.toMessage("outOfStock", "This shop does not have it in stock. Please wait until it's refilled or contact us at %player%.").replace("%player%", Bukkit.getOfflinePlayer(UUID.fromString(owner)).name!!))
+                    return
+                }
+            } else {
+                event.player.sendMessage(main.lang.toMessage("shopNoInventory", "I couldn't buy it because the shop is full. Please wait until the stock is low or contact %player%!").replace("%player%", Bukkit.getOfflinePlayer(UUID.fromString(owner)).name!!))
+                return
+            }
         }
     }
     fun onSignLeftClick(event: PlayerInteractEvent, block: Block, blockState: Sign, data: WallSign) {
